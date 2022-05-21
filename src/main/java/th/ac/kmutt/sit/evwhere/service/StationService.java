2cef08e3c747f2628f3f957e98a2833d12e915b4
Veerapat Prechadech <veerapat.prechadech@decathlon.com>
2022-05-21 11:28:03 +0700

    add distance

--- src/main/java/th/ac/kmutt/sit/evwhere/service/StationService.java
+++ src/main/java/th/ac/kmutt/sit/evwhere/service/StationService.java
@@ -1,12 +1,12 @@
 package th.ac.kmutt.sit.evwhere.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import th.ac.kmutt.sit.evwhere.entity.StationEntity;
 import th.ac.kmutt.sit.evwhere.model.StationResponse;
 import th.ac.kmutt.sit.evwhere.repository.StationRepository;
+import th.ac.kmutt.sit.evwhere.util.DistanceCalculator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +16,8 @@ public class StationService {
 
     @Autowired
     private StationRepository stationRepository;
+    @Autowired
+    private DistanceCalculator distanceCalculator;
 
     public List<StationResponse> getAllStations(int count){
         Flux<StationEntity> stationEntityFlux = stationRepository.findAll();
@@ -29,10 +31,14 @@ public class StationService {
 
     public List<StationResponse> getStationInRadius(Double lat,Double lng){
         List<StationResponse> stationResponses = new ArrayList<>();
-        Flux<StationEntity> stationEntityFlux = stationRepository.getStationByRadius(lat,lng);
+//        Flux<StationEntity> stationEntityFlux = stationRepository.getStationByRadius(lat,lng);
+        Flux<StationEntity> stationEntityFlux = stationRepository.findAll();
         for(StationEntity s : stationEntityFlux.collectList().block()){
             StationResponse st = mapToModel(s);
-            stationResponses.add(st);
+            Double radius = distanceCalculator.distance(lat,lng,s.getLatitude(),s.getLongitude(),'K');
+            if(radius<=20) {
+                stationResponses.add(st);
+            }
         }
         return stationResponses;
     }
